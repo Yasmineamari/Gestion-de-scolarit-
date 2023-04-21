944a4bd059a4ef2c8f6a1d7cd06fc6daf207123e
unknown <yesmineamari698@gmail.com>
2023-04-21 23:37:42 +0100

    sample commit

---
+++ supprimerEtudiant.php
@@ -0,0 +1,144 @@
+<?php
+   session_start();
+   if($_SESSION["autoriser"]!="oui"){
+      header("location:login.php");
+      exit();
+   }
+   
+  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SCO-ENICAR Ajouter Etudiant</title>
+    <!-- Bootstrap core CSS -->
+<link href="./assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap core JS-JQUERY -->
+<script src="./assets/dist/js/jquery.min.js"></script>
+<script src="./assets/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Custom styles for this template -->
+    <link href="./assets/jumbotron.css" rel="stylesheet">
+
+</head>
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">SCO-Enicar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+        
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="index.html" id="dropdown01" data-toggle="dropdown" aria-expanded="false">Gestion des Groupes</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown01">
+                <a class="dropdown-item" href="afficherEtudiants.php">Lister tous les étudiants</a>
+                <a class="dropdown-item" href="afficherEtudiantsParClasse.php">Etudiants par Groupe</a>
+                <a class="dropdown-item" href="#">Ajouter Groupe</a>
+                <a class="dropdown-item" href="#">Modifier Groupe</a>
+                <a class="dropdown-item" href="#">Supprimer Groupe</a>
+      
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-expanded="false">Gestion des Etudiants</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown01">
+                <a class="dropdown-item" href="ajouterEtudiant.php">Ajouter Etudiant</a>
+                <a class="dropdown-item" href="chercherEtudiant.php">Chercher Etudiant</a>
+                <a class="dropdown-item" href="modifierEtudiant.php">Modifier Etudiant</a>
+                <a class="dropdown-item" href="supprimerEtudiant.php">Supprimer Etudiant</a>
+      
+      
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-expanded="false">Gestion des Absences</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown01">
+                <a class="dropdown-item" href="saisirAbsence.php">Saisir Absence</a>
+                <a class="dropdown-item" href="etatAbsence.php">État des absences pour un groupe</a>
+              </div>
+            </li>
+      
+            <li class="nav-item active">
+              <a class="nav-link" href="deconnexion.php">Se Déconnecter <span class="sr-only">(current)</span></a>
+            </li>
+      
+          </ul>
+        
+      
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Saisir un groupe" aria-label="Chercher un groupe">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Chercher Groupe</button>
+          </form>
+        </div>
+      </nav>
+      
+<main role="main">
+        <div class="jumbotron">
+            <div class="container">
+              <h1 class="display-4">supprimer un étudiant</h1>
+              <p>Remplir le formulaire ci-dessous afin de supprimer un étudiant!</p>
+            </div>
+          </div>
+          <div class="container">
+ <form id="myForm" method="POST"  >
+ <div class="form-group">
+     <label for="cin">CIN:</label><br>
+     <input type="text" id="cin" name="cin"  class="form-control" required pattern="[0-9]{8}" title="8 chiffres"/>
+    </div>
+    <button  type="button" onclick="supprimer()" class="btn btn-primary btn-block">supprimer</button>
+    </form> 
+</div>  
+</main>
+<div id="demo"></div>
+<script>
+    function supprimer()
+    {
+        var xmlhttp = new XMLHttpRequest();
+        var url="http://localhost/mini_projet/supprimer.php";
+        
+        //Envoie Req
+        xmlhttp.open("POST",url,true);
+
+        form=document.getElementById("myForm");
+        formdata=new FormData(form);
+
+        xmlhttp.send(formdata);
+
+        //Traiter Res
+
+        xmlhttp.onreadystatechange=function()
+            {   
+                if(this.readyState==4 && this.status==200){
+                // alert(this.responseText);
+                    if(this.responseText=="OK")
+                    {
+                        document.getElementById("demo").innerHTML="La suppression de l'étudiant a été bien effectué";
+                        document.getElementById("demo").style.backgroundColor="green";
+                    }
+                    else
+                    {
+                        document.getElementById("demo").innerHTML="L'étudiant n'existe pas, merci de vérifier le CIN";
+                        document.getElementById("demo").style.backgroundColor="#fba";
+                    }
+                }
+            }
+        
+        
+    }
+    </script>
+
+<footer class="container">
+    <p>&copy; ENICAR 2021-2022</p>
+  </footer>
+
+
+</body>
+</html>
